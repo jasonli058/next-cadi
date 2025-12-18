a558160c519500fd51107201995b757b8c3fbfba
Jason Li <jasonli058@gmail.com>
2025-12-17 21:03:13 -0600

    changed the img tag in ArtCard to next.js Image equivalent

--- src/app/components/ArtCard.js
+++ src/app/components/ArtCard.js
@@ -1,11 +1,21 @@
 "use client"
+import Image from "next/image";
 
 const ArtCard = ({ src, alt}) => (
     <div className='bg-white rounded-xl shadoow-md overflow-hidden flex flex-col items-center'>
-        <img 
+        {/* <img 
             src={src} 
             alt={alt}
             className="w-full h-auto object-cover border border-gray-300 rounded-xl"
+        /> */}
+        <Image 
+            src={src} 
+            alt={alt}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%', height: 'auto' }}
+            className="object-cover border border-gray-300 rounded-xl"
         />
     </div>
 );
